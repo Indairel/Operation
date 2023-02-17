91770f9b631c76411cdf4d87d32504b3dd9253c0
indairel <cyberindygos@gmail.com>
2023-02-17 12:48:25 +0100

    Refactor

---
+++ arithmetic_operators.py
@@ -0,0 +1,9 @@
+do_potegi = (2**5)
+
+ile_jest_w_danej_liczbie = (3750//14)
+
+co_zostalo_po_dzialaniu = (3750%14)
+
+print(do_potegi)
+print(ile_jest_w_danej_liczbie)
+print(co_zostalo_po_dzialaniu)
---
+++ format.py
@@ -0,0 +1,6 @@
+txt = "It would be lovely, if {} could go to Finland in {}".format("I", "this year")
+
+txtwo = "It would be lovely, if {} could go to Finland in {}".format("placeholder", 2023)
+
+print(txt)
+print(txtwo)
