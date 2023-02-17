92cf8f3e721999caac1e71bdacbe4d0be46f65b4
indairel <cyberindygos@gmail.com>
2023-02-17 12:46:07 +0100

    Replace Split

---
+++ replace.py
@@ -0,0 +1,5 @@
+fruits = "Persikka,Apricot,Mustikka,Omena"
+
+new_fruits = fruits.replace("Apricot", "Viniirypälet")
+
+print(new_fruits)
---
+++ split.py
@@ -0,0 +1,11 @@
+txt = "Terve Kuka sina olet"
+
+separate = txt.split()
+
+print(separate)
+
+fruits = "Persikka,Apricot,Mustikka,Omena"
+
+seprarate_fruits = fruits.split(",")
+
+print(seprarate_fruits)
